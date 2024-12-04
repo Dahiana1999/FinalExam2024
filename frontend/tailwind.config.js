7933649d23ce2b23545360bbad472ff558d6a617
DANGO <dahianaprogramacion@gmail.com>
2024-12-04 10:06:37 -0300

    Taildwind configuración final

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Asegúrate de que coincida con tus rutas
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+
